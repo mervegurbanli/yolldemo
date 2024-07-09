5f6a15461d26ad8b2d849c12bc48db4053f2d656
musamehdiyev <m.mehdiyev3473@gmail.com>
2024-07-09 14:24:58 -0400

    some updates

--- src/test/java/musa/M3_TestNG_AP2_test.java
+++ src/test/java/musa/M3_TestNG_AP2_test.java
@@ -1,11 +1,11 @@
 package musa;
 
-import org.junit.Assert;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class M3_TestNG_AP2_test {
 
