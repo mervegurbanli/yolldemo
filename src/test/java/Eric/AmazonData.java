6ecfb97d5affd5a50cc2bb39275a7ecdd8977633
YollEric <es.mglory@gmail.com>
2024-07-09 16:56:56 -0400

    Added Test for amazon footer elements

--- src/test/java/Eric/AmazonData.java
+++ src/test/java/Eric/AmazonData.java
@@ -1,7 +1,5 @@
 package Eric;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public interface AmazonData {
     String[] amazonData = {"Careers", "Amazon Newsletter", "About Amazon", "Accessibility", "Sustainability", "Press Center", "Investor Relations", "Amazon Devices", "Amazon Science", "Sell on Amazon", "Sell apps on Amazon", "Supply to Amazon", "Protect & Build Your Brand", "Become an Affiliate", "Become a Delivery Driver", "Start a Package Delivery Business", "Advertise Your Products", "Self-Publish with Us", "Become an Amazon Hub Partner", "›See More Ways to Make Money", "Amazon Visa", "Amazon Store Card", "Amazon Secured Card", "Amazon Business Card", "Shop with Points", "Credit Card Marketplace", "Reload Your Balance", "Gift Cards", "Amazon Currency Converter", "Your Account", "Your Orders", "Shipping Rates & Policies", "Amazon Prime", "Returns & Replacements", "Manage Your Content and Devices", "Recalls and Product Safety Alerts", "Help"};
