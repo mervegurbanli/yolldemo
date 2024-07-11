0ef10ae6feff70d0513555217bf9fcf5ffdc5554
katimi81 <katimi@gmail.com>
2024-07-11 19:24:51 -0400

    First commit

--- src/test/java/step_definitions/RegisterPageSteps.java
+++ src/test/java/step_definitions/RegisterPageSteps.java
@@ -1,4 +1,44 @@
 package step_definitions;
 
 public class RegisterPageSteps {
+    @Given("the user is on the homepage")
+    public void the_user_is_on_the_homepage() {
+
+    }
+
+    @When("the user clicks on the {string} drop menu")
+    public void the_user_clicks_on_the_drop_menu(String string) {
+
+    }
+
+    @When("the user clicks on the {string} option")
+    public void the_user_clicks_on_the_option(String string) {
+
+    }
+
+    @When("the user enters the following details into the mandatory fields:")
+    public void the_user_enters_the_following_details_into_the_mandatory_fields(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+
+    @When("the user clicks on the {string} button")
+    public void the_user_clicks_on_the_button(String string) {
+
+    }
+
+    @Then("the user should see the {string} page")
+    public void the_user_should_see_the_page(String string) {
+
+    }
+
+    @When("the user clicks on the {string} button on the {string} page")
+    public void the_user_clicks_on_the_button_on_the_page(String string, String string2) {
+
+    }
+
+    @Then("the user should be redirected to the homepage \\(or relevant page post registration)")
+    public void the_user_should_be_redirected_to_the_homepage_or_relevant_page_post_registration() {
+
+    }
+
 }
